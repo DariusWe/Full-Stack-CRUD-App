605ce0ddac59565e48724d08c6ca7a54b8372c68
Darius Weiberg <dariusweiberg@gmail.com>
2023-02-03 12:46:53 +0100

    React Query setup

--- client/src/constants/BASE_URL.js
+++ client/src/constants/BASE_URL.js
@@ -1,6 +1,3 @@
-const BASE_URL =
-  process.env.REACT_APP_ENVIRONMENT === "local"
-    ? "http://localhost:3001"
-    : "https://full-stack-crud-app-production.up.railway.app";
-
-export default BASE_URL;
+export default process.env.REACT_APP_ENVIRONMENT === "local"
+  ? "http://localhost:3001"
+  : "https://full-stack-crud-app-production.up.railway.app";
--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,20 @@
+import "./index.styles.scss";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.styles.scss";
+import { Provider } from "react-redux";
+import { QueryClient, QueryClientProvider } from "react-query";
 import App from "./App";
-import store from './store/store';
-import { Provider } from 'react-redux';
+import store from "./store/store";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
     <Provider store={store}>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </Provider>
   </>
 );
